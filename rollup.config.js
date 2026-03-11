c2dd4002266395e7dc8fdbf8b3425a8a0d89f4e2
Wael <lomriwael4@gmail.com>
2026-03-11 15:31:57 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,16 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import cleanCSS from 'rollup-plugin-clean-css';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
 export default {
-  input: 'src/index.js',
+  input: 'validation.js',
   output: [
     {
-      file: 'public/dist/bundle.js',
+      file: 'dist/bundle.js',
       format: 'iife'
     }
   ],
   plugins: [
     resolve(),
-    commonjs(),
-    cleanCSS({
-      advanced: true
-    })
+    babel({ babelHelpers: 'bundled' })
   ]
 };
